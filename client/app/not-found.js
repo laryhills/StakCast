515565b2abf6c3fbf13f306e78a0a6154399f5b6
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2025-03-21 13:22:53 +0100

    Add files via upload

---
+++ client/app/not-found.js
@@ -0,0 +1,54 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+
+
+export default function NotFound() {
+
+    return (
+        <div className="w-full relative overflow-y-hidden bg-white min-h-screen flex items-center justify-center  flex-col bg-[url('/404Bg.svg')] bg-no-repeat bg-cover " >
+
+
+
+            <div className="absolute top-0 left-[50%] transform translate-x-[-50%] flex items-center justify-center flex-col max-w-[400px] " >
+                <Image src="/404Image.svg" alt="image" height={400} width={400} className='object-cover  ' />
+                <p className=' text-2xl md:text-5xl font-bold text-center whitespace-nowrap  ' >PAGE NOT FOUND</p>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div className='flex flex-col md:flex-row gap-5 md:gap-10 items-center justify-center mt-[18%]  ' >
+                <button className=' w-[200px] h-[40px] bg-gradient-to-r from-green-400 to-blue-500 rounded-3xl text-white text-base font-medium transform hover:scale-105 transition ease-in-out ' >Go Back</button>
+                <button className=' w-[200px] h-[40px] bg-gradient-to-r from-green-400 to-blue-500 rounded-3xl text-white text-base font-medium transform hover:scale-105 transition ease-in-out'>Back to  Home</button>
+            </div>
+
+
+
+
+
+
+
+        </div>
+    )
+}
