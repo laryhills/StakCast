be3b9f690c3b5e9315ad851b634360c9a9bd60a2
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2025-03-22 15:24:54 +0000

    fix

--- client/app/not-found.js
+++ client/app/not-found.js
@@ -1,13 +1,20 @@
+"use client"
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from "next/navigation";
+
 
 
 
 
 export default function NotFound() {
 
+    const router = useRouter();
+
+
     return (
-        <div className="w-full relative overflow-y-hidden bg-white min-h-screen flex items-center justify-center  flex-col bg-[url('/404Bg.svg')] bg-no-repeat bg-cover " >
+        <div className="w-full relative overflow-y-hidden bg-white h-screen flex items-center justify-center  flex-col bg-[url('/404Bg.svg')] bg-no-repeat bg-cover " >
 
 
 
@@ -39,8 +46,11 @@ export default function NotFound() {
 
 
             <div className='flex flex-col md:flex-row gap-5 md:gap-10 items-center justify-center mt-[18%]  ' >
-                <button className=' w-[200px] h-[40px] bg-gradient-to-r from-green-400 to-blue-500 rounded-3xl text-white text-base font-medium transform hover:scale-105 transition ease-in-out ' >Go Back</button>
-                <button className=' w-[200px] h-[40px] bg-gradient-to-r from-green-400 to-blue-500 rounded-3xl text-white text-base font-medium transform hover:scale-105 transition ease-in-out'>Back to  Home</button>
+                <button onClick={() => router.back()} className=' w-[200px] h-[40px] bg-gradient-to-r from-green-400 to-blue-500 rounded-3xl text-white text-base font-medium transform hover:scale-105 transition ease-in-out ' >Go Back</button>
+
+
+
+                <Link href="/" >  <button className=' w-[200px] h-[40px] bg-gradient-to-r from-green-400 to-blue-500 rounded-3xl text-white text-base font-medium transform hover:scale-105 transition ease-in-out'>Back to  Home</button></Link>
             </div>
 
 
