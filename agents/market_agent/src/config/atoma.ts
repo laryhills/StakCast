4716887c27e6fe34b95a7935faa874317aa9d780
Solomon Emmanuel <soulex04292003@gmail.com>
2025-04-11 19:12:13 +0100

    chore:added atomachat

--- agents/market_agent/src/config/atoma.ts
+++ agents/market_agent/src/config/atoma.ts
@@ -1,5 +1,25 @@
 import { AtomaSDK } from "atoma-sdk";
 import config from "./config";
+import prompt from "../prompts/prompt";
+const atomaSdk = new AtomaSDK({ bearerAuth: config.atoma.apiKey });
+async function atomaChat(userMessage: string) {
+  try {
+    const response = await atomaSdk.chat.create({
+      messages: [
+        { role: "assistant", content: prompt() },
+        {
+          role: "user",
+          content: userMessage,
+        },
+      ],
+      model: config.atoma.model || "meta-llama/Llama-3.3-70B-Instruct",
+    });
 
-let atomaSdk = new AtomaSDK({ bearerAuth: config.atoma.apiKey });
+    return response;
+  } catch (error) {
+    console.error("Error during Atoma chat request:", error);
+    throw error;
+  }
+}
 export default atomaSdk;
+export { atomaChat };
