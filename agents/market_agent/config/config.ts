1b6140f1b8ae783c06b13ec6e660c705d8217a67
Solomon Emmanuel <soulex04292003@gmail.com>
2025-04-11 19:03:46 +0100

    chore: finish agent setup

--- agents/market_agent/config/config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  atoma: {
-    apiKey: process.env.ATOMA_KEY,
-    model: process.env.ATOMA_MODEL,
-  },
-  port:process.env.PORT|| 1313,
-  node_env:process.env.NODE_ENV||'development'
-};
